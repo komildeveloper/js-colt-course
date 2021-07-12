77f551d21a3dc42e43eea5f5f1e97b9a6b80024f
komildeveloper <komildeveloper111@gmail.com>
2021-07-12 18:32:46 +0500

    02-else-if branch added

---
+++ app.js
@@ -0,0 +1,9 @@
+let rating = 1;
+
+if (rating === 3) {
+  console.log('YOU ARE SUPERSTAR');
+} else if (rating === 2) {
+  console.log('MEETS EXPECTATIONS');
+} else if (rating === 1) {
+  console.log('NEEDS IMPROVEMENT');
+}
