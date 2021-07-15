9cf0b05b9cd0f6c9e5f55707cd055bb403b9ed3b
komildeveloper <komildeveloper111@gmail.com>
2021-07-15 17:47:55 +0500

    creating arrays

---
+++ arrays/01-creating-arrays.js
@@ -0,0 +1,17 @@
+// An array of strings
+let shoppingList = ['ice', 'cheese', 'cereal'];
+console.log(shoppingList);
+
+// An array of numbers
+let lotto = [23, 67, 92, 45];
+console.log(lotto);
+
+// Mixed array
+let myCollection = [true, 56, 'hello', NaN, false];
+console.log(myCollection);
+
+// Create array
+let myData;
+
+myData = new Array('Hello', true, 77);
+console.log(myData);
