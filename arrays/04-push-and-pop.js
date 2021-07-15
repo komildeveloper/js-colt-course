9acce4b39e90f3d314d62baa8914a3a6a9bc1324
komildeveloper <komildeveloper111@gmail.com>
2021-07-15 17:48:38 +0500

    push-and-pop

---
+++ arrays/04-push-and-pop.js
@@ -0,0 +1,9 @@
+let topSongs = ['First Time', 'Hello', 'No Love', 'GoodBye'];
+
+// push - add to end
+topSongs.push('Balance');
+console.log(topSongs);
+
+// pop - remove from end
+const nextSong = topSongs.pop();
+console.log(nextSong);
