93a2191df65292e856aa82bab9fe97fe44c7d68c
komildeveloper <komildeveloper111@gmail.com>
2021-07-15 18:41:03 +0500

    reverse-and-join added

---
+++ arrays/08-reverse-join.js
@@ -0,0 +1,11 @@
+let letters = ['O', 'L', 'L', 'E', 'H'];
+let uzb = ['N', 'A', 'T', 'S', 'I', 'K', 'E', 'B', 'Z', 'U'];
+let words = ['Air', 'Strike', 'Group'];
+
+// reverse
+console.log(letters.reverse());
+
+// join
+console.log(uzb.join());
+console.log(uzb.reverse().join('.'));
+console.log(words.join());
