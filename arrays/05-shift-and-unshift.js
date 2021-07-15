5cb2082c0ab342d9c735284078e4a13d56f99193
komildeveloper <komildeveloper111@gmail.com>
2021-07-15 17:48:57 +0500

    shift-and-unshift

---
+++ arrays/05-shift-and-unshift.js
@@ -0,0 +1,14 @@
+let cars = ['bmw'];
+
+// unshift - add to start
+cars.unshift('mercedes');
+cars.unshift('toyota', 'chevrolet');
+cars.unshift('lambo');
+console.log(cars);
+
+// shift - remove from start
+cars.shift();
+cars.shift();
+cars.shift();
+cars.shift();
+console.log(cars);
