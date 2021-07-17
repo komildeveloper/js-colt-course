0be6179debd6751c19f5e6c0a3fce058350a0ef1
komildeveloper <komildeveloper111@gmail.com>
2021-07-17 12:31:33 +0500

    accessing object properties

---
+++ objects/03-accessing-object-properties.js
@@ -0,0 +1,18 @@
+/** const numbers = {
+  12: 'hello',
+  33: 'js',
+  '76 trombones': 'hi there'
+};
+
+console.log(numbers[12]);
+console.log(numbers[33]);
+console.log(numbers['76 trombones']); */
+
+const palette = {
+  red: '#eb4d4b',
+  yellow: '#f9ca24',
+  blue: '#30336b'
+};
+
+console.log(palette.red);
+console.log(palette['yellow']);
