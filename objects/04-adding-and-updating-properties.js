7cf35041f1093325dec44f39f82f59aef93e5c03
komildeveloper <komildeveloper111@gmail.com>
2021-07-17 12:31:49 +0500

    adding and updating properties

---
+++ objects/04-adding-and-updating-properties.js
@@ -0,0 +1,9 @@
+const userReviews = {};
+
+userReviews['queenBee49'] = 4.0;
+userReviews.mrSmith78 = 3.5;
+console.log(userReviews);
+
+userReviews['queenBee49'] += 2;
+userReviews.mrSmith78++;
+console.log(userReviews);
