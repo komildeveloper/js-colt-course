1430056c9ba32c409e6af022567c4ba7f0e86fc4
komildeveloper <komildeveloper111@gmail.com>
2021-07-17 12:32:20 +0500

    objects and reference types

---
+++ objects/06-objects-and-reference-types.js
@@ -0,0 +1,10 @@
+const palette = {
+  red: '#eb4d4b',
+  yellow: '#f9ca24',
+  blue: '#30336b'
+};
+
+const palette2 = palette;
+palette2.green = '#ebf876';
+console.log(palette);
+console.log(palette2);
