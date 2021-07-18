782be1e8b8b17f2066a50d4a6485977f57545d0a
komildeveloper <komildeveloper111@gmail.com>
2021-07-18 15:37:31 +0500

    for loops added

---
+++ 04-loops/01-for-loops.js
@@ -0,0 +1,39 @@
+// FOR LOOPS
+/**
+
+for() {
+  [initialExperssion],
+  [condition],
+  [incrementExperssion]
+}
+
+*/
+
+// 10 total Times
+
+// 50 initial value
+// 60 when to run the loop
+// +1 how to change value each time
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`HELLO: ${i}`);
+// }
+
+// 1 1x1
+// 2 2x2
+// 3 3x3
+// 4 4x4
+// 5 5x5
+// 6 6x6
+// 7 7x7
+// 8 8x8
+// 9 9x9
+// 10 10x10
+
+for (let num = 1; num <= 20; num++) {
+  console.log(`${num}x${num} = ${num * num}`);
+}
+
+for (let i = 200; i >= 0; i -= 25) {
+  console.log(i);
+}
