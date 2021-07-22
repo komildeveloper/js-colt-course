6f6e6eeeeafb52165ea77919925bb68af377ab13
komildeveloper <komildeveloper111@gmail.com>
2021-07-22 21:52:56 +0500

    functions with multiple arguments added

---
+++ 05-functions/04-functions-with-multiple-arguments.js
@@ -0,0 +1,17 @@
+function square(num) {
+  console.log(num * num);
+}
+
+square(5);
+
+function sum(x, y) {
+  console.log(x + y);
+}
+
+sum(5, 15);
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+divide(100, 2);
