817175efa013b342b192a82eca237fc748a4e0da
komildeveloper <komildeveloper111@gmail.com>
2021-07-22 21:51:55 +0500

    our first function added

---
+++ 05-functions/01-our-first-function.js
@@ -0,0 +1,32 @@
+// Functions last "big" topic
+
+// GOALS
+/**
+ * Write valid functions
+ * Write functions with arguments
+ * Compare function experssion & function statements
+ * Write a bunch of function statements!
+ */
+
+// DEFINE
+/**
+  function funcName() {
+    // do something
+  }
+*/
+
+// RUN
+/**
+ * funcName(); // run once
+ * funcName(); // run again!
+ */
+
+function grumpus() {
+  console.log('ugh...you again...');
+  console.log('FOR THE LAST TIME...');
+  console.log('LEAVE MEN ALONE!!!');
+}
+
+for (let i = 0; i < 50; i++) {
+  grumpus();
+}
