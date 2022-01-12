044e724434a66ddea86dbfc06d07ee2ff2b70d14
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-12 14:41:09 +0500

    if statement added

--- 01_controlling_programm_logic_and_flow/app.js
+++ 01_controlling_programm_logic_and_flow/app.js
@@ -1,1 +1,15 @@
-console.log(3 + 4, 'hello', true);
+if(1 !== 1) {
+  console.log("It's True!");
+}
+
+// Example 2
+let rating = 3;
+if(rating === 3) {
+  console.log('YOU ARE A SUPERSTAR!');
+}
+
+// Example 3
+let num = 37;
+if(num % 2 !== 0) {
+  console.log('ODD NUMBER!');
+}
