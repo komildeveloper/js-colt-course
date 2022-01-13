446baeb667b40a71ce06f0a95b141c3494b2903e
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-13 15:23:19 +0500

    indexOf & includes added

--- 02_arrays/app.js
+++ 02_arrays/app.js
@@ -1,10 +1,27 @@
-// concat
-let fruits = ['apple', 'banana'];
-let veggies = ['asparagus', 'brussel spourts'];
-let meats = ['steak', 'chicken breast'];
+let ingredients = [
+  'water',
+  'corn starch',
+  'flour',
+  'cheese',
+  'brown sugar',
+  'shrimp',
+  'eel',
+  'butter'
+]
 
-console.log(fruits.concat(veggies));
-console.log(veggies.concat(fruits));
+// includes
+console.log(ingredients.includes('fish'));
+console.log(ingredients.includes('water'));
+console.log(ingredients.includes('corn'));
 
-let allFoods = fruits.concat(veggies, meats);
-console.log(allFoods)
+console.log(ingredients.includes('flour', 2));
+
+if(ingredients.includes('flour')) {
+  console.log('I AM GLUTTEN FREE, I CANNOT EAT THAT!');
+}
+
+// indexOf
+console.log(ingredients.indexOf('cheese'));
+console.log(ingredients.indexOf('bread'));
+
+console.log(ingredients.indexOf('cheese', 2));
