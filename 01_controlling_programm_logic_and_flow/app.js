7e6c4ce4bdc849ab68595374fd19cefad509684c
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-13 14:39:04 +0500

    ternary operator added

--- 01_controlling_programm_logic_and_flow/app.js
+++ 01_controlling_programm_logic_and_flow/app.js
@@ -1,62 +1,21 @@
-// SWITCH STATEMENT
-let day = 3;
+let num = 2;
 
-/* if(day === 1) {
-  console.log('MONDAY')
-} else if(day === 2) {
-  console.log('TUESDAY')
-} else if(day === 3) {
-  console.log('WEDNESDAY')
-} else if(day === 4) {
-  console.log('THURSDAY')
-} else if(day === 5) {
-  console.log('FRIDAY')
-} else if(day === 6) {
-  console.log('SATURDAY')
-} else if(day === 7) {
-  console.log('SUNDAY')
+/* if(num === 7) {
+  console.log('LUCKY!');
 } else {
-  console.log('INVALID DAY');
+  console.log('BAD!');
 } */
 
-/* switch (day) {
-	case 1:
-		console.log('MONDAY');
-		break;
-	case 2:
-		console.log('TUESDAY');
-		break;
-	case 3:
-		console.log('WEDNESDAY');
-		break;
-	case 4:
-		console.log('THURSDAY');
-		break;
-	case 5:
-		console.log('FRIDAY');
-		break;
-	case 6:
-		console.log('SATURDAY');
-		break;
-	case 7:
-		console.log('SUNDAY');
-		break;
-	default:
-		console.log('INVALID DAY');
+// num === 7 ? console.log('LUCKY!') : console.log('BAD!');
+
+let status = 'online';
+/* let color;
+
+if(status === 'offline') {
+  color = 'red'
+} else {
+  color = 'green'
 } */
 
-let emoji = 'lips';
-
-switch (emoji) {
-  case 'sad face':
-  case 'happy face':
-    console.log('yellow')
-    break;
-  case 'eggplant':
-    console.log('purple')
-    break;
-  case 'heart':
-  case 'lips':
-    console.log('red')
-    break;
-}
+let color = status === 'offline' ? 'red' : 'green';
+console.log(color);
