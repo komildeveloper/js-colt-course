ede454de782b5ed3e6b0d72729c607302b6461a4
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-13 14:49:56 +0500

    array indicies

--- 02_arrays/app.js
+++ 02_arrays/app.js
@@ -1,6 +1,6 @@
-// creating arrays
-let shoppingList = ['cereal', 'ice', 'cheese']
-
-let lotto = [45, 12, 23, 25, 34];
-
-let myCollection = [12, 'dog', true, null, undefined, NaN]
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+console.log(colors.length);
+console.log(colors[1]);
+console.log(colors[3]);
+console.log(colors[0]);
+console.log(colors[colors.length - 1]);
