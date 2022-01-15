460709c21018fb61b8b67f81cf739ff8406c0c0f
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-15 15:08:24 +0500

    Array.sort() added

--- 02_arrays/app.js
+++ 02_arrays/app.js
@@ -1,25 +1,8 @@
-// Array.splice()
-let animals = [ 'shark', 'salmon', 'whale', 'bear', 'lizard', 'tortoise' ];
+// Array.sort()
+let people = ['Mrs. Robinson', 'Angie', 'Jolene', 'Maggie May', 'Roxanne']
 
-//splice(startIdx, deleteCount, itemsToInsert);
+console.log(people); // unsorted
+console.log(people.sort()); // sorted
 
-// =====================
-// INSERTING w/ SPLICE
-// =====================
-// 'at index 1, delete 0 items and insert "octopus"'
-animals.splice(1, 0, 'octopus');
-//["shark", "octopus", "salmon", "whale", "bear", "lizard", "tortoise"]
-
-// =====================
-// DELETING w/ SPLICE
-// =====================
-//'at index 5, delete 2 items'
-animals.splice(5, 2);
-//["shark", "octopus", "salmon", "whale", "bear"]
-
-// =====================
-// REPLACING w/ SPLICE
-// =====================
-//'at index 3, delete 2 items and replace them with "orca" and "grizzly"'
-animals.splice(3, 2, 'orca', 'grizzly');
-// ["shark", "octopus", "salmon", "orca", "grizzly"]
+let nums = [34, 10, 100000, 67, 99];
+console.log(nums.sort());
