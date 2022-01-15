f31bab9b5d2c1451dd61aaa7ce43567bb26ff5e7
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-15 14:53:29 +0500

    Array.slice() added

--- 02_arrays/app.js
+++ 02_arrays/app.js
@@ -1,7 +1,21 @@
-let letters = ['T', 'C', 'E', 'P', 'S', 'E', 'R'];
+// Array.slice()
+let animals = ['shark', 'salmon', 'whale', 'bear', 'lizard', 'tortoise']
 
-// reverse
-console.log(letters.reverse());
+let swimmers = animals.slice(0, 3)
+console.log(swimmers)
 
-// join
-console.log(letters.reverse().join())
+let mammals = animals.slice(2, 4)
+console.log(mammals)
+
+// let reptiles = animals.slice(4, 6)
+let reptiles = animals.slice(4)
+console.log(reptiles)
+
+let quadruped = animals.slice(-3)
+let quadruped2 = animals.slice(-3, -1)
+console.log(quadruped)
+console.log(quadruped2)
+
+// copy
+let copy = animals.slice();
+console.log(copy);
