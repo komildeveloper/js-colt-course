028542386769f4b441dc47b7fae11e8ed5c7469d
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-15 16:12:31 +0500

    nested arrays added

--- 02_arrays/app.js
+++ 02_arrays/app.js
@@ -1,8 +1,9 @@
-// Array.sort()
-let people = ['Mrs. Robinson', 'Angie', 'Jolene', 'Maggie May', 'Roxanne']
+// Nested Arrays
+const colors = [
+  ['red', 'crismon'],
+  ['orange', 'dark orange'],
+  ['yellow', 'golden rod'],
+]
 
-console.log(people); // unsorted
-console.log(people.sort()); // sorted
-
-let nums = [34, 10, 100000, 67, 99];
-console.log(nums.sort());
+colors[2][0] = 'green'
+console.log(colors[2]);
