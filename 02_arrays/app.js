7ab268c1a6763a0c63eda8213e080c46b3cd43eb
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-15 14:33:01 +0500

    reverse and join added

--- 02_arrays/app.js
+++ 02_arrays/app.js
@@ -1,27 +1,7 @@
-let ingredients = [
-  'water',
-  'corn starch',
-  'flour',
-  'cheese',
-  'brown sugar',
-  'shrimp',
-  'eel',
-  'butter'
-]
+let letters = ['T', 'C', 'E', 'P', 'S', 'E', 'R'];
 
-// includes
-console.log(ingredients.includes('fish'));
-console.log(ingredients.includes('water'));
-console.log(ingredients.includes('corn'));
+// reverse
+console.log(letters.reverse());
 
-console.log(ingredients.includes('flour', 2));
-
-if(ingredients.includes('flour')) {
-  console.log('I AM GLUTTEN FREE, I CANNOT EAT THAT!');
-}
-
-// indexOf
-console.log(ingredients.indexOf('cheese'));
-console.log(ingredients.indexOf('bread'));
-
-console.log(ingredients.indexOf('cheese', 2));
+// join
+console.log(letters.reverse().join())
