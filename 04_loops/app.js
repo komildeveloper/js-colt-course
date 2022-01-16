ea490c2d87cbb378f7e65c77de8b6386a7f6f7fc
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-16 23:51:17 +0500

    nested for loops added

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -1,47 +1,23 @@
-// looping arrays
-// const examScores = [98, 77, 84, 91, 57, 66];
-
-/* for(let i = 0; i < examScores.length; i++) {
-  console.log(i, examScores[i]);
+/* for (let i = 1; i <= 10; i++) {
+  console.log(`OUTER LOOP: ${i}`);
+  for (let j = 10; j >= 0; j -= 2) {
+    console.log(` INNER LOOP: ${j}`);
+  }
 } */
 
-const myStudents = [
-  {
-    firstName: 'Zeus',
-    grade: 86
-  },
-  {
-    firstName: 'Artemis',
-    grade: 97
-  },
-  {
-    firstName: 'Hera',
-    grade: 72
-  },
-  {
-    firstName: 'Apollo',
-    grade: 90
-  }
+const gameBoard = [
+  [4, 32, 8, 4],
+  [64, 8, 32, 2],
+  [8, 32, 16, 4],
+  [2, 8, 4, 2]
 ];
 
-/* for(let i = 0; i < myStudents.length; i++) {
-  let student = myStudents[i]
-  console.log(`${student.firstName} scored ${student.grade}`);
-}; */
-
 let total = 0;
-for(let i = 0; i < myStudents.length; i++) {
-  let student = myStudents[i]
-  total += student.grade;
-};
-
-console.log(total / myStudents.length);
-
-/* const word = 'stressed';
-let reversedWord = '';
-
-for(let i = word.length - 1; i >= 0; i--) {
-  reversedWord += word[i]
-  // console.log(reversedWord);
+for (let i = 0; i < gameBoard.length; i++) {
+  let row = gameBoard[i]
+  for (let j = 0; j < row.length; j++) {
+    total += row[j]
+  }
 }
-console.log(reversedWord); */
+
+console.log(total);
