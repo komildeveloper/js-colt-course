5bc9f48b597a042261a02cb263aa0414721db0bd
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-16 23:57:37 +0500

    while loop added

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -1,23 +1,10 @@
-/* for (let i = 1; i <= 10; i++) {
-  console.log(`OUTER LOOP: ${i}`);
-  for (let j = 10; j >= 0; j -= 2) {
-    console.log(` INNER LOOP: ${j}`);
-  }
+/* for (let i = 0; i <= 5; i++) {
+  console.log(i);
 } */
 
-const gameBoard = [
-  [4, 32, 8, 4],
-  [64, 8, 32, 2],
-  [8, 32, 16, 4],
-  [2, 8, 4, 2]
-];
-
-let total = 0;
-for (let i = 0; i < gameBoard.length; i++) {
-  let row = gameBoard[i]
-  for (let j = 0; j < row.length; j++) {
-    total += row[j]
-  }
+// while loop
+let j = 0;
+while(j <= 10) {
+  console.log(j);
+  j++;
 }
-
-console.log(total);
