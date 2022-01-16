45b61321f17c1b22649ef3fe7f383d5c8463b878
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-17 02:15:50 +0500

    break keyword added

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -1,4 +1,4 @@
-const target = Math.floor(Math.random() * 10);
+/* const target = Math.floor(Math.random() * 10);
 let guess = Math.floor(Math.random() * 10);
 
 while(guess !== target) {
@@ -10,4 +10,15 @@ console.log(`Target: ${target} Guess: ${guess}`);
 console.log('CONGRATS YOU WIN!');
 
 // while (some condition)
-// in the loop, update or attempt to make that condition false
+// in the loop, update or attempt to make that condition false */
+const target = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10);
+
+while(true) {
+  if(target === guess) break;
+  console.log(`Target: ${target} Guess: ${guess}`);
+  guess = Math.floor(Math.random() * 10);
+}
+
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log('CONGRATS YOU WIN!');
