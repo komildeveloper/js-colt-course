4721c90a0f9115da8c66d4e14833ff592de273dd
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-17 01:10:20 +0500

    more while loops added

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -1,10 +1,13 @@
-/* for (let i = 0; i <= 5; i++) {
-  console.log(i);
-} */
+const target = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10);
 
-// while loop
-let j = 0;
-while(j <= 10) {
-  console.log(j);
-  j++;
+while(guess !== target) {
+  console.log(`Target: ${target} Guess: ${guess}`);
+  guess = Math.floor(Math.random() * 10);
 }
+
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log('CONGRATS YOU WIN!');
+
+// while (some condition)
+// in the loop, update or attempt to make that condition false
