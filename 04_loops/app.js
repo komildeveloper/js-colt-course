65a40ba3a5c53457c6ea6b693ee2a3a30df5cd00
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-17 14:27:20 +0500

    comparing for and for...of

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -1,13 +1,29 @@
-let subreddits = ['soccer', 'popheades', 'cringe', 'books'];
+const magicSquare = [
+  [2, 7, 6],
+  [9, 5, 1],
+  [4, 3, 8]
+];
 
-/*for(let i = 0; i < subreddits.length; i++) {
-  console.log(subreddits[i]);
+/* for(let i = 0; i < magicSquare.length; i++) {
+  let row = magicSquare[i]
+  let sum = 0;
+  for(let j = 0; j < row.length; j++) {
+    sum += row[j]
+  }
+  console.log(`${row} summed to ${sum}`);
 } */
 
-for(let sub of subreddits) {
-  console.log(sub);
-}
+/* for(let row of magicSquare) {
+  let sum = 0;
+  for(let num of row) {
+    sum += num;
+  }
+  console.log(`${row} summed to ${sum}`);
+} */
 
-for(let char of 'football') {
-  console.log(char.toUpperCase());
+const words1 = ['mail', 'milk', 'bath', 'black'];
+const words2 = ['box', 'shake', 'tub', 'berry'];
+
+for(let i = 0; i < words1.length; i++) {
+  console.log(`${words1[i]}${words2[i]}`);
 }
