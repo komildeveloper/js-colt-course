dd326ed3691b42cc9f350349a8d1a25d989426a6
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-17 14:42:06 +0500

    for...of in objects

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -1,29 +1,31 @@
-const magicSquare = [
-  [2, 7, 6],
-  [9, 5, 1],
-  [4, 3, 8]
-];
-
-/* for(let i = 0; i < magicSquare.length; i++) {
-  let row = magicSquare[i]
-  let sum = 0;
-  for(let j = 0; j < row.length; j++) {
-    sum += row[j]
-  }
-  console.log(`${row} summed to ${sum}`);
-} */
-
-/* for(let row of magicSquare) {
-  let sum = 0;
-  for(let num of row) {
-    sum += num;
-  }
-  console.log(`${row} summed to ${sum}`);
-} */
-
-const words1 = ['mail', 'milk', 'bath', 'black'];
-const words2 = ['box', 'shake', 'tub', 'berry'];
-
-for(let i = 0; i < words1.length; i++) {
-  console.log(`${words1[i]}${words2[i]}`);
+const movieReviews = {
+  Arrival: 9.5,
+  Alien: 9,
+  Amelie: 8,
+  'In Bruges': 9,
+  Amadeus: 10,
+  'Kill Bill': 8,
+  'Little Miss Sunshine': 8.5,
+  Coraline: 7.5,
 }
+
+for(let movie of Object.keys(movieReviews)) {
+  console.log(movie, movieReviews[movie]);
+}
+
+const ratings = Object.values(movieReviews);
+
+let total = 0;
+for(let r of ratings) {
+  total += r;
+}
+
+/*
+total /= ratings.length
+console.log(total);
+*/
+
+let avg = total / ratings.length;
+console.log(avg);
+
+// console.log(Object.keys(movieReviews));
