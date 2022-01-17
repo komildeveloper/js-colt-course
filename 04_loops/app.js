ad935f9059f89f3c6ef7392a3c637a175a62961d
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-17 13:51:45 +0500

    updated app.js

--- 04_loops/app.js
+++ 04_loops/app.js
@@ -11,14 +11,14 @@ console.log('CONGRATS YOU WIN!');
 
 // while (some condition)
 // in the loop, update or attempt to make that condition false */
-const target = Math.floor(Math.random() * 10);
-let guess = Math.floor(Math.random() * 10);
+const target = Math.floor(Math.random() * 10)
+let guess = Math.floor(Math.random() * 10)
 
-while(true) {
-  if(target === guess) break;
-  console.log(`Target: ${target} Guess: ${guess}`);
-  guess = Math.floor(Math.random() * 10);
+while (true) {
+	if (target === guess) break
+	console.log(`Target: ${target} Guess: ${guess}`)
+	guess = Math.floor(Math.random() * 10)
 }
 
-console.log(`Target: ${target} Guess: ${guess}`);
-console.log('CONGRATS YOU WIN!');
+console.log(`Target: ${target} Guess: ${guess}`)
+console.log('CONGRATS YOU WIN!')
